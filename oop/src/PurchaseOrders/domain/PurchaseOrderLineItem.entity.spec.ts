4198e14941970b5d00cf0a538d9a537d3ab720bc
Nick Ostreim <nostreim@gmail.com>
2024-04-30 13:40:39 -0700

    Add line item details

--- oop/src/PurchaseOrders/domain/PurchaseOrderLineItem.entity.spec.ts
+++ oop/src/PurchaseOrders/domain/PurchaseOrderLineItem.entity.spec.ts
@@ -8,8 +8,16 @@ describe("Purcase Order Entity", () => {
     const lineItem = new PurchaseOrderLineItem({
       id: createUuid(),
       purchaseOrderId: purchaseOrder.id,
+      itemNumber: "F-123",
+      description: "A fizzbob for the debopulator",
+      price: 1000.15,
+      quantity: 1,
     });
     expect(isUuid(lineItem.id)).toBeTruthy();
     expect(isUuid(lineItem.purchaseOrderId)).toBeTruthy();
+    expect(lineItem.itemNumber).toBe("F-123");
+    expect(lineItem.description).toBe("A fizzbob for the debopulator");
+    expect(lineItem.price).toBe(1000.15);
+    expect(lineItem.quantity).toBe(1);
   });
 });
--- oop/src/PurchaseOrders/domain/PurchaseOrderLineItem.entity.ts
+++ oop/src/PurchaseOrders/domain/PurchaseOrderLineItem.entity.ts
@@ -3,9 +3,31 @@ import { UUID, createUuid } from "../../utilities/uuid";
 export class PurchaseOrderLineItem {
   private _id: UUID;
   private _purchaseOrderId: UUID;
-  constructor({ id, purchaseOrderId }: { id?: UUID; purchaseOrderId: UUID }) {
+  private _itemNumber: string;
+  private _description: string;
+  private _price: number;
+  private _quantity: number;
+  constructor({
+    id,
+    purchaseOrderId,
+    itemNumber,
+    description,
+    price,
+    quantity,
+  }: {
+    id?: UUID;
+    purchaseOrderId: UUID;
+    itemNumber: string;
+    description: string;
+    price: number;
+    quantity: number;
+  }) {
     this._id = id ?? createUuid();
     this._purchaseOrderId = purchaseOrderId;
+    this._itemNumber = itemNumber;
+    this._description = description;
+    this._price = price;
+    this._quantity = quantity;
   }
 
   get id() {
@@ -14,5 +36,21 @@ export class PurchaseOrderLineItem {
 
   get purchaseOrderId() {
     return this._purchaseOrderId;
+  }
+
+  get itemNumber() {
+    return this._itemNumber;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  get quantity() {
+    return this._quantity;
   }
 }

92ebfa4be56372a516e2a4eeead2eb3987ebf66a
Nick Ostreim <nostreim@gmail.com>
2024-05-14 13:59:22 -0700

    Use entity

--- oop/src/PurchaseOrders/use-cases/CreatePO.usecase.ts
+++ oop/src/PurchaseOrders/use-cases/CreatePO.usecase.ts
@@ -3,6 +3,7 @@ import { UUID, createUuid } from "../../utilities/uuid";
 import { IPORepository } from "../domain/PurchaseOrder.repo-interface";
 import { PurchaseOrder } from "../domain/PurchaseOrder.entity";
 import { CreatePOLineItemDTO } from "../infrastructure/dto/CreatePO.dto";
+import { PurchaseOrderLineItem } from "../domain/PurchaseOrderLineItem.entity";
 
 type CreatePOProps = {
   id: UUID;
@@ -13,14 +14,17 @@ export class CreatePO implements UseCase<CreatePOProps> {
   async execute({ id, lineItems }: CreatePOProps) {
     const purchaseOrder = new PurchaseOrder({
       id,
-      lineItems: lineItems?.map((lineItem) => ({
-        id: createUuid(),
-        itemNumber: lineItem.itemNumber,
-        description: lineItem.description,
-        price: lineItem.price,
-        quantity: lineItem.quantity,
-        purchaseOrderId: id,
-      })),
+      lineItems: lineItems?.map(
+        (lineItem) =>
+          new PurchaseOrderLineItem({
+            id: createUuid(),
+            purchaseOrderId: id,
+            itemNumber: lineItem.itemNumber,
+            description: lineItem.description,
+            price: lineItem.price,
+            quantity: lineItem.quantity,
+          })
+      ),
     });
     await this.repo.save(purchaseOrder);
     return purchaseOrder.id;

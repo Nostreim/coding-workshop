5e65a69fe4e53438bca7760ab806a9d7a7933483
Nick Ostreim <nostreim@gmail.com>
2024-05-14 13:32:41 -0700

    Move repo & add line items

--- oop/src/PurchaseOrders/infrastructure/repositories/PORepository.ts
+++ oop/src/PurchaseOrders/infrastructure/repositories/PORepository.ts
@@ -1,6 +1,6 @@
-import { UUID } from "../../utilities/uuid";
-import { IPORepository } from "./IPORepository";
-import { PurchaseOrder } from "./PurchaseOrder.entity";
+import { UUID } from "../../../utilities/uuid";
+import { IPORepository } from "../../domain/IPORepository";
+import { PurchaseOrder } from "../../domain/PurchaseOrder.entity";
 
 export class PORepository implements IPORepository {
   purchaseOrders: PurchaseOrder[] = [];
--- oop/src/PurchaseOrders/use-cases/CreatePO.usecase.ts
+++ oop/src/PurchaseOrders/use-cases/CreatePO.usecase.ts
@@ -1,12 +1,28 @@
 import { UseCase } from "../../lib/UseCase.base";
+import { UUID, createUuid } from "../../utilities/uuid";
 import { IPORepository } from "../domain/IPORepository";
 import { PurchaseOrder } from "../domain/PurchaseOrder.entity";
+import { CreatePOLineItemDTO } from "../infrastructure/dto/CreatePO.dto";
 
-type CreatePOProps = {};
+type CreatePOProps = {
+  id: UUID;
+  lineItems: CreatePOLineItemDTO[];
+};
 export class CreatePO implements UseCase<CreatePOProps> {
   constructor(private repo: IPORepository) {}
-  async execute() {
-    const purchaseOrder = new PurchaseOrder();
+  async execute({ id, lineItems }: CreatePOProps) {
+    const purchaseOrderId = createUuid();
+    const purchaseOrder = new PurchaseOrder({
+      id: purchaseOrderId,
+      lineItems: lineItems.map((lineItem) => ({
+        id: createUuid(),
+        itemNumber: lineItem.itemNumber,
+        description: lineItem.description,
+        price: lineItem.price,
+        quantity: lineItem.quantity,
+        purchaseOrderId,
+      })),
+    });
     await this.repo.save(purchaseOrder);
     return purchaseOrder.id;
   }

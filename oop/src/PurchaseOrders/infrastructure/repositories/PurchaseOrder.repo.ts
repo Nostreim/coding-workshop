8a40376689afb565c13b575781e4964b6ee40c86
Nick Ostreim <nostreim@gmail.com>
2024-06-11 14:53:12 -0700

    Adjust save method

--- oop/src/PurchaseOrders/infrastructure/repositories/PurchaseOrder.repo.ts
+++ oop/src/PurchaseOrders/infrastructure/repositories/PurchaseOrder.repo.ts
@@ -5,7 +5,10 @@ import { PurchaseOrder } from "../../domain/PurchaseOrder.entity";
 export class PORepository implements IPORepository {
   purchaseOrders: PurchaseOrder[] = [];
   async save(po: PurchaseOrder) {
-    this.purchaseOrders.push(po);
+    const existingPO = this.purchaseOrders.find((p) => p.id === po.id);
+    this.purchaseOrders = existingPO
+      ? [...this.purchaseOrders.filter((p) => p.id !== po.id), po]
+      : [...this.purchaseOrders, po];
     return undefined;
   }
   async fetch(id: UUID) {

fde7d8f0efd6fb29bdbf9ad5b70cda1e540f9a15
Nick Ostreim <nostreim@gmail.com>
2024-06-11 13:25:12 -0700

    Add prefix validation

--- oop/src/PurchaseOrders/domain/PONumber.value-object.spec.ts
+++ oop/src/PurchaseOrders/domain/PONumber.value-object.spec.ts
@@ -5,4 +5,10 @@ describe("PONumber", () => {
     const poNumber = new PONumber("syn-123");
     expect(poNumber).toBeDefined();
   });
+
+  it("throws an error if the value is not prefixed with 'syn-'", () => {
+    expect(() => new PONumber("123" as `syn-${number}`)).toThrow(
+      "Purchase Order Number must be prefixed with 'syn-'"
+    );
+  });
 });
--- oop/src/PurchaseOrders/domain/PONumber.value-object.ts
+++ oop/src/PurchaseOrders/domain/PONumber.value-object.ts
@@ -1,6 +1,10 @@
 export class PONumber {
   private _value: `syn-${number}`;
   constructor(value: `syn-${number}`) {
+    if (!value.startsWith("syn-")) {
+      throw new Error("Purchase Order Number must be prefixed with 'syn-'");
+    }
+
     this._value = value;
   }
   get value() {

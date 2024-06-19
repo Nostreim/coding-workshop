e0a83d0a61c852ae5893046e83878027532644d4
Nick Ostreim <nostreim@gmail.com>
2024-06-19 08:17:03 -0700

    Clean up

--- oop/src/PurchaseOrders/infrastructure/repositories/PurchaseOrder.repo.ts
+++ oop/src/PurchaseOrders/infrastructure/repositories/PurchaseOrder.repo.ts
@@ -10,10 +10,10 @@ const byPONumber = (a: PurchaseOrder, b: PurchaseOrder) => {
 export class PORepository implements IPORepository {
   purchaseOrders: PurchaseOrder[] = [];
   async save(po: PurchaseOrder) {
-    const existingPO = this.purchaseOrders.find((p) => p.id === po.id);
-    this.purchaseOrders = existingPO
-      ? [...this.purchaseOrders.filter((p) => p.id !== po.id), po]
-      : [...this.purchaseOrders, po];
+    this.purchaseOrders = [
+      ...this.purchaseOrders.filter((p) => p.id !== po.id),
+      po,
+    ];
     return undefined;
   }
   async fetch(id: UUID) {

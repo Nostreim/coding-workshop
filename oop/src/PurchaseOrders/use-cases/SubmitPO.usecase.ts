052f734c67547a1d1173f51aab1266ef9f868884
Nick Ostreim <nostreim@gmail.com>
2024-08-06 13:21:07 -0700

    Move business logic to entity

--- oop/src/PurchaseOrders/use-cases/SubmitPO.usecase.ts
+++ oop/src/PurchaseOrders/use-cases/SubmitPO.usecase.ts
@@ -1,22 +1,31 @@
 import { UseCase } from "../../lib/UseCase.base";
+import { EntityMissing } from "../../utilities/error/EntityMissing";
+import { UUID } from "../../utilities/uuid";
 import { PONumber } from "../domain/PONumber.value-object";
 import { PurchaseOrder } from "../domain/PurchaseOrder.entity";
 import { IPORepository } from "../domain/PurchaseOrder.repo-interface";
 
 type SubmitPOProps = {
-  purchaseOrder: PurchaseOrder;
+  purchaseOrderId: UUID;
 };
 
 export class SubmitPO implements UseCase<SubmitPOProps> {
   constructor(private repo: IPORepository) {}
-  async execute({ purchaseOrder }: { purchaseOrder: PurchaseOrder }) {
+  async execute({ purchaseOrderId }: SubmitPOProps) {
+    const purchaseOrder = await this.repo.fetch(purchaseOrderId);
+
+    if (!purchaseOrder) {
+      throw new EntityMissing(
+        `Purchase Order with ID ${purchaseOrderId} not found`
+      );
+    }
+
     const lastPO = await this.repo.fetchLastPOByNumber();
-    await this.repo.save(
-      new PurchaseOrder({
-        id: purchaseOrder.id,
-        number: lastPO?.number?.increment() ?? new PONumber("syn-000001"),
-        lineItems: purchaseOrder.lineItems,
-      })
-    );
+    const nextPONumber =
+      lastPO?.number?.increment() ?? new PONumber("syn-000001");
+
+    purchaseOrder?.submit(nextPONumber);
+
+    await this.repo.save(purchaseOrder);
   }
 }
